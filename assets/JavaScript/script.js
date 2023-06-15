cdd89cafd7a5a7fd3e426595aeb9a2e889c53bba
faiezPapaSiddhi <faiez.ahmed@papasiddhi.com>
2023-06-15 18:21:39 +0530

    new code

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -10,18 +10,18 @@
 
 
 
-const slidesContainer = document.querySelector(".slides-container");
-const slideWidth = slidesContainer.querySelector(".slide").clientWidth;
-const prevButton = document.querySelector(".prev");
-const nextButton = document.querySelector(".next");
+// const slidesContainer = document.querySelector(".slides-container");
+// const slideWidth = slidesContainer.querySelector(".slide").clientWidth;
+// const prevButton = document.querySelector(".prev");
+// const nextButton = document.querySelector(".next");
 
-nextButton.addEventListener("click", () => {
-	slidesContainer.scrollLeft += slideWidth;
-});
+// nextButton.addEventListener("click", () => {
+// 	slidesContainer.scrollLeft += slideWidth;
+// });
 
-prevButton.addEventListener("click", () => {
-	slidesContainer.scrollLeft -= slideWidth;
-});
+// prevButton.addEventListener("click", () => {
+// 	slidesContainer.scrollLeft -= slideWidth;
+// });
 
 
 
@@ -98,4 +98,5 @@ var swiper = new Swiper('.mySwiper', {
       };
   }
 
-
+// Current Tab Active
+// Get all tab links
